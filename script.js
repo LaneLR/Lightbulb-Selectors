6e6009e154ac25cac26269ec8c3cb55407c7333a
LaneLR <laneleerichardson@gmail.com>
2025-04-30 16:04:33 -0500

    fixed line 4 of script.js to be a querySelector instead of getElementByClassName

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const bulb1 = document.getElementById("lightbulb1");
 const bulb2 = document.getElementById("lightbulb2");
 const bulb3 = document.getElementById("lightbulb3");
-const counter = document.getElementsByClassName("subtitle");
+const counter = document.querySelector(".subtitle");
 let count = 0;
 
 
